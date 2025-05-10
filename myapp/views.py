6b76dc9b6c18f1141a7e0cc2df2d0437e0ee5a1c
ZhinHemn <153916638+ZhinHemn@users.noreply.github.com>
2025-05-10 21:36:31 +0300

    Delete myapp directory

--- myapp/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-def home(request):
-    return HttpResponse("Welcome to my website, my name is Zhin, from Computer Science, in University of Sulaymaniyah!")
